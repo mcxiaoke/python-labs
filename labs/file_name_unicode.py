a719d78372efaa252468bf952d565987cc64075e
mcxiaoke <mcxiaoke@gmail.com>
2015-07-10 13:59:44 +0800

    autopep8

--- labs/file_name_unicode.py
+++ labs/file_name_unicode.py
@@ -3,13 +3,15 @@
 # @Author: mcxiaoke
 # @Date:   2015-07-09 20:46:37
 
-import os,sys
+import os
+import sys
 from os import path
+
 
 def handler(arg, dirname, names):
     print "process dir: %s" % dirname
     for name in names:
-        filename=path.join(dirname,name)
+        filename = path.join(dirname, name)
         print "process file: %s" % filename
 
 print sys.getdefaultencoding()
@@ -17,11 +19,11 @@ print sys.stdin.encoding
 print sys.stdout.encoding
 print sys.stderr.encoding
 
-if len(sys.argv)<2:
+
+if len(sys.argv) < 2:
     sys.exit(1)
 
 # 如果这里不提前转换为unicode
 # 在Sublime的console里无法显示中文文件名
-root=unicode(path.abspath(sys.argv[1]))
-os.path.walk(root,handler,())
-
+root = unicode(path.abspath(sys.argv[1]))
+os.path.walk(root, handler, ())

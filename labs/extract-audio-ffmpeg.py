dbd803d5f020e3f5d19902686a907eb65756bbbb
mcxiaoke <mcxiaoke@live.com>
2017-06-28 17:30:48 +0800

    update extract-audio-ffmpeg.py

--- labs/extract-audio-ffmpeg.py
+++ labs/extract-audio-ffmpeg.py
@@ -13,6 +13,15 @@ import shutil
 import subprocess
 from os import path
 
+VIDEO_FORMATS = ('.mp4', '.mkv', '.avi', '.wmv', '.flv', '.f4v', '.mpg', '.ts')
+
+
+def get_audio_codec(fi):
+    args = "ffprobe -v error -select_streams a:0 " \
+        "-show_entries stream=codec_name -of " \
+        "default=noprint_wrappers=1:nokey=1 {}".format(fi).split()
+    return subprocess.check_output(args)
+
 
 def process(curdir, name):
     # subprocess.check_call(["ls", "-l"])
@@ -21,11 +30,24 @@ def process(curdir, name):
     fi = path.join(curdir, name)
     fo = os.path.join(curdir, '{}.m4a'.format(base))
     print('input: {}'.format(fi))
+    if ext.lower() not in VIDEO_FORMATS:
+        return
+    if os.path.exists(fo) and os.path.getsize(fo) > 0:
+        return
+    acodec = get_audio_codec(fi)
+    # print('input audio codec: {}'.format(acodec))
     print('output: {}'.format(fo))
-    # subprocess.call(["file", f])
-    if name.lower().endswith('.mp4') and not os.path.exists(fo):
-        args = "ffmpeg -i {} -vn -c:a copy {}".format(fi, fo).split()
-        subprocess.call(args, stderr=subprocess.STDOUT)
+    # args = "ffmpeg -i {} -vn -c:a copy {}".format(fi, fo).split()
+    # -c:a libfdk_aac -b:a 128k
+    if acodec and acodec.strip() == 'aac':
+        print('just copy original audio stream')
+        args = "ffmpeg -hide_banner -v error -i {} -vn -c:a copy {}".format(
+            fi, fo).split()
+    else:
+        print('need convert original audio stream')
+        args = "ffmpeg -hide_banner -v error -i {} -vn -c:a libfdk_aac -vbr 4 {}".format(
+            fi, fo).split()
+    subprocess.call(args, stderr=subprocess.STDOUT)
 
 
 def main(root):
@@ -33,6 +55,7 @@ def main(root):
         print(u'-- {} --'.format(curdir))
         for name in filenames:
             process(curdir, name)
+
 
 if __name__ == '__main__':
     print(sys.argv)

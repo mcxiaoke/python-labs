6932636acfc752d89c5ea0fbf00a13a2e1375c3d
mcxiaoke <mcxiaoke@live.com>
2017-12-20 14:13:56 +0800

    add image_resize.py

---
+++ labs/remove_duplciate_files.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import os
+import sys
+import shutil
+
+def by_size():
+    '''match file type and size'''
+    pass
+
+def by_hash():
+    '''match file content sha1'''
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

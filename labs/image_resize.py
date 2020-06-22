d3116da84d98557df2f1952cd903cd92554b3b7c
mcxiaoke <mcxiaoke@live.com>
2020-06-22 10:36:18 +0800

    image resize script

--- labs/image_resize.py
+++ labs/image_resize.py
@@ -2,19 +2,24 @@
 # -*- coding: utf-8 -*-
 # @Author: mcxiaoke
 # @Date:   2015-12-11 17:03:40
-from __future__ import print_function
 import os
+import time
+from os import path
 import sys
 import shutil
 import re
 from PIL import Image
+from multiprocessing.dummy import Pool
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor, as_completed
+from threading import current_thread
 
 MAX_WIDTH = 1600  # 1200 1600
+EXTENSIONS = ('.jpg', '.png', '.gif', '.tiff')
 
 
 def resize_one(src, dst, max_width):
     _, ext = os.path.splitext(os.path.basename(src))
-    if not ext or ext.lower() not in ['.jpg', '.png', '.gif']:
+    if not ext or ext.lower() not in EXTENSIONS:
         print("Not image: {}".format(dst))
         return
     _, ext = os.path.splitext(os.path.basename(dst))
@@ -50,6 +55,96 @@ def resize_batch(src, dst, max_width):
         resize_one(old_file, new_file, max_width)
 
 
+def clear_thumbs(src_dir):
+    for root, _, files in os.walk(src_dir):
+        for name in files:
+            if 'thumb' in name:
+                os.remove(os.path.join(root, name))
+                print('Remove thumb: {}'.format(os.path.join(root, name)))
+    for root, dirs, _ in os.walk(src_dir):
+        for adir in dirs:
+            if 'thumb' in adir:
+                os.rmdir(os.path.join(root, adir))
+                print('Clear thumbs: {}'.format(os.path.join(root, adir)))
+
+
+def make_thumb_one(src, dst):
+    max_width = MAX_WIDTH
+    file_name = os.path.basename(src)
+    fbase, ext = os.path.splitext(file_name)
+    # print("{},{},{}".format(src, dst, file_name))
+    if not ext or ext.lower() not in ['.jpg', '.png', '.gif']:
+        print("Not image: {}".format(dst))
+        return
+    dst = os.path.join(dst, '{}_thumb{}'.format(fbase, ext))
+    if not os.path.exists(src):
+        print("Not exists: {}".format(src))
+        return
+    if os.path.exists(dst):
+        print("Exists: {}".format(dst))
+        return
+    try:
+        im = Image.open(src)
+        width, height = im.size
+        if width > max_width:
+            # print("SRC: {} {}".format(src, im.size))
+            if width > height:
+                nw = max_width
+                nh = int((float(height) * nw / float(width)))
+            else:
+                nh = max_width
+                nw = int((float(width) * nh / float(height)))
+            nim = im.resize((nw, nh))
+            nim.save(dst, quality=85)
+            print("DST: {} {} ({})".format(dst, nim.size, os.getpid()))
+            return dst
+    except IOError as e:
+        print(e)
+
+
+def make_thumb_one_args(args):
+    make_thumb_one(args[0], args[1])
+
+
+def make_thumbs(src_dir):
+    images = []
+    for root, dirs, files in os.walk(src_dir):
+        for adir in dirs:
+            if 'thumb' in adir:
+                continue
+        for name in files:
+            if 'thumb' in name:
+                continue
+            _, ext = path.splitext(name)
+            if not ext or ext.lower() not in EXTENSIONS:
+                continue
+            rel_path = path.join(root, name)
+            src_path = path.abspath(rel_path)
+            dst_path = path.join(root, 'thumbs')
+            if not os.path.exists(dst_path):
+                os.makedirs(dst_path)
+            images.append((src_path, dst_path))
+    start = time.time()
+    # with ThreadPoolExecutor(max_workers=4) as executor:
+    #     futures = {executor.submit(make_thumb_one, src, dst): (
+    #         src, dst) for (src, dst) in images}
+    #     for future in as_completed(futures):
+    #         try:
+    #             result = future.result()
+    #         except Exception as e:
+    #             print(e)
+    with Pool(4) as p:
+        # for (src, dst) in images:
+            # print("Process:", src_path)
+            # make_thumb_one(src, dst)
+            # p.apply_async(make_thumb_one, (src, dst))
+        p.map(make_thumb_one_args, images)
+        p.close()
+        p.join()
+    elapsed = time.time()-start
+    print("Task finished, using {} seconds.".format(elapsed))
+
+
 def main():
     if len(sys.argv) < 2:
         print('Usage: python %s src [dst] [width]' % sys.argv[0])
@@ -82,4 +177,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # clear_thumbs(sys.argv[1])
+    make_thumbs(sys.argv[1])

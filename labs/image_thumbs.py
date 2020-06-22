3cdda4259cb88e879e781271385429ee1d720b10
mcxiaoke <mcxiaoke@live.com>
2020-06-22 19:22:47 +0800

    image thumbs tweak

--- labs/image_thumbs.py
+++ labs/image_thumbs.py
@@ -68,6 +68,12 @@ def clear_thumbs(src_dir):
                 print('Clear thumbs: {}'.format(os.path.join(root, adir)))
 
 
+def get_thumb_filename(src):
+    file_name = os.path.basename(src)
+    fbase, ext = os.path.splitext(file_name)
+    return '{}_thumb{}'.format(fbase, ext)
+
+
 def make_thumb_one(src, dst):
     max_width = MAX_WIDTH
     file_name = os.path.basename(src)
@@ -76,7 +82,7 @@ def make_thumb_one(src, dst):
     if not ext or ext.lower() not in ['.jpg', '.png', '.gif']:
         print("Not image: {}".format(dst))
         return
-    dst = os.path.join(dst, '{}_thumb{}'.format(fbase, ext))
+    dst = os.path.join(dst, get_thumb_filename(src))
     if not os.path.exists(src):
         print("Not exists: {}".format(src))
         return
@@ -102,6 +108,22 @@ def make_thumb_one(src, dst):
         print(e)
 
 
+def move_thumbs(src_dir):
+    thumbs = []
+    for root, dirs, files in os.walk(src_dir):
+        for adir in dirs:
+            if 'thumb' in adir:
+                src = os.path.join(root, adir)
+                dst = src.replace('/Photos/',
+                                  '/Photos/Thumbs/')
+                thumbs.append((src, dst))
+                print(dst)
+    for src, dst in thumbs:
+        print('SRC: {}'.format(src))
+        print('DST: {}'.format(dst))
+        shutil.move(src, dst)
+
+
 def make_thumb_one_args(args):
     make_thumb_one(args[0], args[1])
 
@@ -110,20 +132,24 @@ def make_thumbs(src_dir):
     images = []
     for root, dirs, files in os.walk(src_dir):
         for adir in dirs:
-            if 'thumb' in adir:
+            if 'thumb' in adir.lower():
                 continue
         for name in files:
-            if 'thumb' in name:
+            if 'thumb' in name.lower():
                 continue
             _, ext = path.splitext(name)
             if not ext or ext.lower() not in EXTENSIONS:
                 continue
-            rel_path = path.join(root, name)
-            src_path = path.abspath(rel_path)
+            src_path = path.abspath(path.join(root, name))
             dst_path = path.join(root, 'thumbs')
+            dst_file = path.join(dst_path, get_thumb_filename(src_path))
             if not os.path.exists(dst_path):
                 os.makedirs(dst_path)
-            images.append((src_path, dst_path))
+            if os.path.exists(dst_file):
+                print("Skip: {}".format(dst))
+                continue
+             images.append((src_path, dst_path))
+    total = len(images)
     start = time.time()
     # with ThreadPoolExecutor(max_workers=4) as executor:
     #     futures = {executor.submit(make_thumb_one, src, dst): (
@@ -142,7 +168,7 @@ def make_thumbs(src_dir):
         p.close()
         p.join()
     elapsed = time.time()-start
-    print("Task finished, using {} seconds.".format(elapsed))
+    print("Task finished, {} files, using {} seconds.".format(total, elapsed))
 
 
 def main():
@@ -177,5 +203,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # clear_thumbs(sys.argv[1])
+    # move_thumbs(sys.argv[1])
     make_thumbs(sys.argv[1])
